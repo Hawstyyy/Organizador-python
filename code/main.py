1f3dc735e3c29bd499374c8187406422c303836f
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-07-15 13:02:46 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import os
 import shutil as sh
-
+print(os.getcwd())
 
 exten = []
 pastas = []
@@ -22,7 +22,7 @@ for nome in exten:
   if nome not in os.listdir(path):
     os.mkdir(f"{path}/{nome}")
   else:
-    print("ja tem")
+    print("- Pasta já presente")
 
 for arquivo in os.listdir(path):
   arq_path = os.path.abspath(f"{path}/{arquivo}")
@@ -30,14 +30,4 @@ for arquivo in os.listdir(path):
     caminho, tipo = os.path.splitext(arq_path)
     sh.move(arq_path, f"{path}/{tipo}")
 
-for pasta in os.listdir(path):
-  pasta_path = os.path.abspath(f"{path}/{pasta}")
-  if os.path.isdir(pasta_path) == True:
-    nome = os.path.basename(pasta_path)
-    banco_de_dados = open("extensoes.txt", "r")
-    print(banco_de_dados.readlines())
-    if nome in banco_de_dados.readlines():
-      continue
-    else:
-      sh.move(pasta_path, f"{path}/Pasta")
-      banco_de_dados.close()
+print("Encerrando...")
