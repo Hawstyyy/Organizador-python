aa89ee265aa8e5103d320749896a54cb099b181a
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-07-16 16:28:52 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,16 +1,22 @@
 import os
+import time
 import shutil as sh
+from tkinter import *
+from tkinter import filedialog
 
-
+root = Tk()
+root.withdraw()
+path = filedialog.askdirectory()
 exten = []
 pastas = []
-path = input(r"- Insira o diretório: ")
 
 try:
   dir_pasta = os.mkdir(f"{path}/Pastas")
   print("Executando...")
+  time.sleep(3)
 except:
   print("Executando...")
+  time.sleep(3)
 
 for arquivo in os.listdir(path):
   arq_path = os.path.abspath(f"{path}/{arquivo}")
@@ -20,12 +26,18 @@ for arquivo in os.listdir(path):
 
 for nome in exten:
   if nome not in os.listdir(path):
+    print(f"Criando pasta: {nome}")
+    time.sleep(2)
     os.mkdir(f"{path}/{nome}")
   else:
-    print("ja tem")
+    print(f"Pasta {nome} já presente")
 
 for arquivo in os.listdir(path):
   arq_path = os.path.abspath(f"{path}/{arquivo}")
   if os.path.isfile(arq_path) == True:
     caminho, tipo = os.path.splitext(arq_path)
     sh.move(arq_path, f"{path}/{tipo}")
+
+print("Finalizando...")
+time.sleep(3)
+print("Pronto!")
