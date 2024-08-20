1bdfd92e8f2a45ccfa555f3eea3175894b135412
Enzo Lopez <enzo54099426@aluno.ms.senac.br>
2024-08-20 10:02:24 -0400

    adjustments on the butttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import shutil as sh
 from tkinter import *
-from tkinter import ttk,filedialog
+from tkinter import ttk,filedialog,messagebox
 
 exten = []
 
@@ -30,11 +30,7 @@ def organizar():
       caminho, tipo = os.path.splitext(arq_path)
       sh.move(arq_path, f"{folder_selected}/{tipo}")
 
-  top= Toplevel(root)
-  top.geometry("200x100")
-  top.title("Popup")
-  Label(top, text= "Pasta organizada!", font=("Arial", 12)).place(relx=0.5, rely=0.4, anchor=CENTER)
-  Button(top, text= "Close", command=top.destroy, font=("Arial", 12)).place(relx=0.5, rely=0.7, anchor=CENTER)
+  messagebox.showinfo("Concluído", "Pasta organizada!")
 
 root = Tk()
 
@@ -46,12 +42,10 @@ ttk.Label(root, text="Pasta selecionada:", font=("Arial",20, "bold")).place(relx
 folder_label = ttk.Label(root, font=("Arial", 10))
 folder_label.place(relx=0.5, rely=0.35, anchor=CENTER)
 
-btn1 = Button(root, text="Selecionar pasta", command=diretorio, height=2, width=20)
+btn1 = ttk.Button(root, text="Selecionar pasta", command=diretorio, )
 btn1.place(relx=0.5, rely=0.5, anchor=CENTER)
-btn1.config(background="green", foreground="white", font=("Arial", 10))
 
-btn2 = Button(root, text="Organizar pasta", command=organizar, height=2, width=20)
+btn2 = ttk.Button(root, text="Organizar pasta", command=organizar)
 btn2.place(relx=0.5, rely=0.65, anchor=CENTER)
-btn2.config(background="green", foreground="white", font=("Arial", 10))
 
 root.mainloop()
