3459dcb53cedba3c082c88a7e0b2fb877ee6d2f3
Enzo Guenka Lopez <hawstyyy@proton.me>
2024-08-21 09:16:02 -0400

    Finishing CTk implementation

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import shutil as sh
 import customtkinter
-from customtkinter import filedialog
-from tkinter import messagebox
+import customtkinter as ctk
+from tkinter import messagebox,filedialog
 
 exten = []
 
 def diretorio():
   global folder_selected
   folder_selected = filedialog.askdirectory()
-  folder_label.config(text=folder_selected)
+  folder_label.configure(text=folder_selected)
 
 def organizar():
   for arquivo in os.listdir(folder_selected):
@@ -33,3 +33,21 @@ def organizar():
 
   messagebox.showinfo("Concluído", "Pasta organizada!")
 
+root = ctk.CTk()
+
+root.title("Python Organizer")
+root.geometry("400x300")
+root.resizable(False, False)
+
+texto = ctk.CTkLabel(root, text="Pasta selecionada:", font=("Arial",20, "bold"), text_color="white")
+texto.place(relx=0.5, rely=0.2, anchor="center")
+folder_label = ctk.CTkLabel(root, font=("Arial", 10), text="Nenhuma")
+folder_label.place(relx=0.5, rely=0.35, anchor="center")
+
+btn1 = ctk.CTkButton(root, text="Selecionar pasta", command=diretorio, width=250, height=35)
+btn1.place(relx=0.5, rely=0.5, anchor="center")
+
+btn2 = ctk.CTkButton(root, text="Organizar pasta", command=organizar, width=250, height=35)
+btn2.place(relx=0.5, rely=0.65, anchor="center")
+
+root.mainloop()
